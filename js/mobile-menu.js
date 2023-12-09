fd55fa8b5565d8863b25ab9c07f33cb8ffcfbe84
Vladislav <91179551+11Vladislav@users.noreply.github.com>
2023-12-09 10:01:46 +0200

    about, prices, reasons - tab and mob add

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -12,27 +12,3 @@
     refs.menu.classList.toggle('is-hidden');
   }
 })();
-
-// const menuLinks = document.querySelectorAll('.nav-link[data-goto]');
-// if (menuLinks.length > 0) {
-//     menuLinks.forEach(menuLink => {
-//         menuLink.addEventListener("click", onMenuLinkClick);
-//     });
-// function onMenuLinkClick(e) {
-//         const menuLink = e.target;
-//         if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-//             const gotoBlock = document.querySelector(menuLink.dataset.goto);
-//             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY; 
-
-//             if (document.querySelector('[data-mobile-menu]').classList.contains('is-hidden')) {
-//                 document.querySelector('[data-mobile-menu]').classList.remove('is-hidden');
-//             }
-
-//             window.scrollTo({
-//                 top: gotoBlockValue,
-//                 behavior: "smooth"
-//             });
-//             e.preventDefault();
-//         }
-//     }
-// }
