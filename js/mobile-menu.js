ad734c082d7d28feeafddff3c92f0f91212ab30d
Vladislav <91179551+11Vladislav@users.noreply.github.com>
2023-12-13 12:33:28 +0200

    mob menu and modal fix

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,10 +3,12 @@
     openMenuBtn: document.querySelector('[data-mobile-menu-open]'),
     closeMenuBtn: document.querySelector('[data-mobile-menu-close]'),
     menu: document.querySelector('[data-mobile-menu]'),
+    backDrop: document.querySelector('.backdrop')
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.backDrop.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
